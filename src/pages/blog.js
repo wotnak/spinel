ea06542f6112ed1e9b2fda9e3becb4e657ca8a32
wotnak <adamwotnak@gmail.com>
2019-09-08 13:28:55 +0200

    add blog page

---
+++ src/pages/blog.js
@@ -0,0 +1,39 @@
+import React from "react"
+import { StaticQuery, Link, graphql } from "gatsby"
+import Layout from '../components/layout.js'
+
+const Post = ({ post }) => (
+  <article>
+    <h2><Link to={`/blog/${post.slug}`}>{post.title}</Link></h2>
+    <div dangerouslySetInnerHTML={{__html: post.excerpt}} />
+  </article>
+)
+
+const Blog = ({ posts }) => {
+  return (
+    <Layout>
+      <h1>Blog firmowy</h1>
+      {posts.map(post => (<Post post={post}/>))}
+    </Layout>
+  )
+}
+
+export default props => (
+  <StaticQuery
+    query={graphql`
+      query {
+        allWordpressPost {
+          edges {
+            node {
+              id
+              slug
+              title
+              excerpt
+            }
+          }
+        }
+      }
+    `}
+    render={data => <Blog posts={data.allWordpressPost.edges.map(edge => edge.node)} {...props} />}
+  />
+)
