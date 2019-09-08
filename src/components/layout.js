3ad64a70faeacd85659e50349320a6e756f17101
wotnak <adamwotnak@gmail.com>
2019-09-08 11:48:56 +0200

    add main menu

--- src/components/layout.js
+++ src/components/layout.js
@@ -11,6 +11,7 @@ import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
 import "./layout.css"
+import Menu from './menu'
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -26,6 +27,7 @@ const Layout = ({ children }) => {
   return (
     <>
       <Header siteTitle={data.site.siteMetadata.title} />
+      <Menu />
       <div
         style={{
           margin: `0 auto`,
---
+++ src/components/menu.js
@@ -0,0 +1,34 @@
+import { StaticQuery, Link, graphql } from "gatsby"
+import React from "react"
+
+const MenuLevel = ({ items }) => (
+  <ul>
+    { items.map( item => (
+      <li>
+        <Link to={item.url}>{item.title}</Link>
+        { item.children ? <MenuLevel items={item.children} /> : null }
+      </li>
+    ) ) }
+  </ul>
+)
+
+export default props => (
+  <StaticQuery
+    query={graphql`
+      query {
+        menu: wordpressWpApiMenusMenusItems(slug: {eq: "menu-glowne"}) {
+          items {
+            order
+            title
+            url
+            children: wordpress_children {
+              title
+              url
+            }
+          }
+        }
+      }
+    `}
+    render={data => <MenuLevel items={data.menu.items} {...props} />}
+  />
+)
