3a6a8ee7f00c122ca0927e061a3c07eee1545970
wotnak <adamwotnak@gmail.com>
2019-09-08 11:48:31 +0200

    add blog posts

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,28 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
-
-// You can delete this file if you're not using it
+const path = require(`path`)
+const slash = require(`slash`)
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+  // query content for WordPress posts
+  const result = await graphql(`
+    query {
+      allWordpressPost {
+        edges {
+          node {
+            id
+            slug
+          }
+        }
+      }
+    }
+  `)
+  const postTemplate = path.resolve(`./src/templates/post.js`)
+  result.data.allWordpressPost.edges.forEach(edge => {
+    createPage({
+      path: `/blog/${edge.node.slug}`,
+      component: slash(postTemplate),
+      context: {
+        id: edge.node.id,
+      },
+    })
+  })
+}
---
+++ src/templates/post.js
@@ -0,0 +1,25 @@
+import React from "react"
+import { graphql } from "gatsby"
+import Layout from '../components/layout.js'
+
+const PostTemplate = (props) => {
+  const post = props.data.wordpressPost
+  return (
+    <Layout>
+      <h1 dangerouslySetInnerHTML={{ __html: post.title }} />
+      <div dangerouslySetInnerHTML={{ __html: post.content }} />
+    </Layout>
+  )
+}
+
+
+export default PostTemplate
+
+export const pageQuery = graphql`
+  query($id: String!) {
+    wordpressPost(id: { eq: $id }) {
+      title
+      content
+    }
+  }
+`
