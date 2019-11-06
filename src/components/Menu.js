77d2412eb2942ee34df0c4cffe348d7125bff97c
wotnak <adamwotnak@gmail.com>
2019-11-06 19:50:25 +0100

    finish site menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,16 +1,39 @@
 import { StaticQuery, Link, graphql } from "gatsby"
-import React from "react"
+import React, { useState }from "react"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 
-const MenuLevel = ({ items }) => (
-  <ul>
-    { items.map( item => (
-      <li>
-        <Link to={item.url.replace('https://spinel.pl','')}>{item.title}</Link>
-        { item.children ? <MenuLevel items={item.children} /> : null }
-      </li>
-    ) ) }
-  </ul>
-)
+const MenuLevel = ({ items }) => {
+  const [expanded, setExpanded] = useState(false);
+  return (
+    <ul>
+      { items.map( (item, i) => {
+        let url = item.url.replace('https://spinel.pl','')
+        if (url.trim().length === 0) url = false
+        return (
+          <li key={i} className={expanded ? 'expanded' : ''}>
+            { url !== false ?
+                <Link to={url}>{item.title}</Link>
+              :
+                <a onClick={() => setExpanded(!expanded)}>{item.title}</a>
+            }
+            { item.children ? <MenuLevel items={item.children} /> : null }
+          </li>
+        )
+      })}
+    </ul>
+  )
+}
+
+const Menu = ({data}) => {
+  const [visible, setVisible] = useState(false);
+  return (
+    <nav className={`site-menu ${visible ? 'visible' : ''}`}>
+      <button onClick={() => setVisible(!visible)}><FontAwesomeIcon icon={faBars} /> Menu</button>
+      <MenuLevel items={data.menu.items} />
+    </nav>
+  )
+}
 
 export default props => (
   <StaticQuery
@@ -18,10 +41,10 @@ export default props => (
       query {
         menu: wordpressWpApiMenusMenusItems(slug: {eq: "menu-glowne"}) {
           items {
-            order
             title
             url
             children: wordpress_children {
+              object_id
               title
               url
             }
@@ -29,11 +52,7 @@ export default props => (
         }
       }
     `}
-    render={data => (
-      <nav className="site-menu">
-        <button>Menu</button>
-        <MenuLevel items={data.menu.items} {...props} />
-      </nav>
-    )}
+    
+    render={data => (<Menu data={data} {...props} />)}
   />
 )
