67a4d8af917f7286418cdceadd6310e5606b0dc4
wotnak <adamwotnak@gmail.com>
2020-02-13 08:15:20 +0100

    improve header background image loading

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,39 @@
 import React from "react"
+import { StaticQuery, graphql } from "gatsby"
+import BackgroundImage from 'gatsby-background-image'
 
-const Header = ({ siteTitle, isFrontPage }) => (
-  <header className={`header${isFrontPage ? " header--home-page" : ""}`}>
-    <h1 dangerouslySetInnerHTML={{ __html: siteTitle.replace('|', '<br/>') }} />
-    {isFrontPage && (
-      <p>
-        Jesteśmy polską firmą rodzinną, działamy nieprzerwanie od 1988 roku.
-        <br />
-        Zajmujemy się kompleksowo hydrauliką siłową i pneumatyką.
-      </p>
-    )}
-  </header>
+export default ({ siteTitle, isFrontPage }) => (
+  <StaticQuery
+    query={graphql`
+      query {
+        bg: file(relativePath: { eq: "headbg.png" }) {
+          childImageSharp {
+            fluid(maxWidth: 998) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    `}
+    render={data => {
+      // Set ImageData.
+      const imageData = data.bg.childImageSharp.fluid
+      return (
+        <BackgroundImage
+          Tag="header"
+          className={`header${isFrontPage ? " header--home-page" : ""}`}
+          fluid={imageData}
+        >
+          <h1 dangerouslySetInnerHTML={{ __html: siteTitle.replace('|', '<br/>') }} />
+          {isFrontPage && (
+            <p>
+              Jesteśmy polską firmą rodzinną, działamy nieprzerwanie od 1988 roku.
+              <br />
+              Zajmujemy się kompleksowo hydrauliką siłową i pneumatyką.
+            </p>
+          )}
+        </BackgroundImage>
+      )
+    }}
+  />
 )
-
-export default Header
