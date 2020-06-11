924b9405fe2cc793d7a61b0438b158a6d1985df5
wotnak <adamwotnak@gmail.com>
2020-06-11 17:25:57 +0200

    display latest blog post on home page only if it was posted in last 30 days

--- src/components/LatestBlogPostBlock.js
+++ src/components/LatestBlogPostBlock.js
@@ -7,9 +7,11 @@ export default () => {
         latestPost: wordpressPost(status: { eq: "publish" }) {
           title
           path
+          date(difference: "now")
         }
       }
   `)
+  if (latestPost.date/1000/60/60/24/30 > 1) return (<></>)
   return (
     <aside className="block--latest-post">
       Ostatnio na blogu: <Link to={latestPost.path}>{latestPost.title}</Link>
