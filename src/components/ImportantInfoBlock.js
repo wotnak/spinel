7b6bd51730491ec548e218ddb478825f61d7c9f1
wotnak <adamwotnak@gmail.com>
2021-05-04 19:37:21 +0200

    change covid info message

--- src/components/ImportantInfoBlock.js
+++ src/components/ImportantInfoBlock.js
@@ -3,8 +3,7 @@ import React from "react"
 const ImportantInfoBlock = () => (
   <>
   <aside className="block--important-info">
-    Prosimy Klientów o wchodzenie do biura pojedynczo i zachowanie bezpiecznej odległości 1.5m.<br/>
-    <strong>Prosimy o  zachowanie odległości od mechanika 2m.</strong><br/>
+    Prosimy Klientów o wchodzenie do biura pojedynczo i zachowanie bezpiecznej odległości 2m od mechanika.<br/>
     Po wyjściu każdego klienta dezynfekujemy klamki i powierzchnie.
   </aside>
   </>
